4818b0d51d01d28dbcb219b7a5b26e83a12964e6
Manu-Freitas <manuela.c.freitas@aluno.senai.br>
2025-09-22 10:38:35 -0300

    feat: adiciona cartasRoutes.js

---
+++ src/controllers/routes/cartasRoutes.js
@@ -0,0 +1,17 @@
+import express from "express";
+import {
+  getAllCartas,
+  getCartasById,
+  createCartas,
+  deleteCartas, 
+  updateCarta } from "../controllers/cartasController.js";
+
+const router = express.Router();
+
+router.get("/", getAllCartas);
+router.get("/:id", getCartasById);
+router.post("/", createCartas);
+router.delete("/:id", deleteCartas);
+router.put("/:id", updateCarta);
+
+export default router;
