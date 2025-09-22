48d365af9e5188afc6cb7875796e4afb1e6ace4f
Manu-Freitas <manuela.c.freitas@aluno.senai.br>
2025-09-22 10:35:02 -0300

    feat: adiciona daos no arquivo dados.js

---
+++ src/controllers/models/dados.js
@@ -0,0 +1,114 @@
+const cartas = [
+    {
+      id: 1,
+      nomeCarta: "Charizard",
+      raridade: "Ultra Rara",
+      edicao: "Base Set",
+      preco: 1500.00,
+      jogo: "Base Set",
+      tipo: "Fogo",
+      poder: 120,
+      condicao: "Excelente"
+    },
+    {
+      id: 2,
+      nomeCarta: "Pikachu",
+      raridade: "Comum",
+      edicao: "Jungle",
+      preco: 5.00,
+      jogo: "Jungle",
+      tipo: "Elétrico",
+      poder: 60,
+      condicao: "Bom"
+    },
+    {
+      id: 3,
+      nomeCarta: "Mewtwo",
+      raridade: "Rara",
+      edicao: "Fossil",
+      preco: 50.00,
+      jogo: "Fossil",
+      tipo: "Psíquico",
+      poder: 90,
+      condicao: "Perfeito"
+    },
+    {
+      id: 4,
+      nomeCarta: "Blastoise",
+      raridade: "Rara",
+      edicao: "Base Set",
+      preco: 200.00,
+      jogo: "Base Set",
+      tipo: "Água",
+      poder: 100,
+      condicao: "Jogado"
+    },
+    {
+      id: 5,
+      nomeCarta: "Venusaur",
+      raridade: "Rara",
+      edicao: "Base Set",
+      preco: 180.00,
+      jogo: "Base Set",
+      tipo: "Grama",
+      poder: 100,
+      condicao: "Excelente"
+    },
+    {
+      id: 6,
+      nomeCarta: "Umbreon VMAX",
+      raridade: "Secreta Rara",
+      edicao: "Evolving Skies",
+      preco: 800.00,
+      jogo: "Evolving Skies",
+      tipo: "Escuridão",
+      poder: 310,
+      condicao: "Perfeito"
+    },
+    {
+      id: 7,
+      nomeCarta: "Gengar",
+      raridade: "Holográfica",
+      edicao: "Fossil",
+      preco: 45.00,
+      jogo: "Fossil",
+      tipo: "Psíquico",
+      poder: 80,
+      condicao: "Bom"
+    },
+    {
+      id: 8,
+      nomeCarta: "Snorlax",
+      raridade: "Incomum",
+      edicao: "Jungle",
+      preco: 15.00,
+      jogo: "Jungle",
+      tipo: "Incolor",
+      poder: 90,
+      condicao: "Jogado"
+    },
+    {
+      id: 9,
+      nomeCarta: "Lugia",
+      raridade: "Rara",
+      edicao: "Neo Genesis",
+      preco: 120.00,
+      jogo: "Neo Genesis",
+      tipo: "Psíquico",
+      poder: 90,
+      condicao: "Excelente"
+    },
+    {
+      id: 10,
+      nomeCarta: "Rayquaza V",
+      raridade: "V Rara",
+      edicao: "Evolving Skies",
+      preco: 90.00,
+      jogo: "Evolving Skies",
+      tipo: "Dragão",
+      poder: 210,
+      condicao: "Perfeito"
+    }
+  ];
+  
+  export default cartas;
